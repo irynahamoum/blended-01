ce44f22d56ac0e6ae479cad86c0c5f45c1068a33
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-03-09 15:54:34 +0200

    Second task

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,33 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
 
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+const icons = [
+  <FaRegThumbsUp key="0" />,
+  <MdPeople key="1" />,
+  <MdOutlineProductionQuantityLimits key="2" />,
+  <GiTreeDoor key="3" />,
+];
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <>
+      {title && <h3 className={style.title}>{title}</h3>}
+
+      <ul className={style.list}>
+        {stats.map((stat, index) => {
+          return (
+            <StatisticsItem
+              key={stat.id}
+              total={stat.total}
+              title={stat.title}
+              icon={icons[index]}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
 };
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,15 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './Statistics.module.css';
+
+import { IconContext } from 'react-icons';
+
+export const StatisticsItem = ({ total, title, icon }) => {
+  return (
+    <li className={style.item}>
+      <IconContext.Provider value={{ size: 30, color: 'brown' }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </li>
+  );
 };
